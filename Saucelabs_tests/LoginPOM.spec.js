80c61413113f397d5d87384d73995858b4c0730f
sudeepinib <sudeepinib@gmail.com>
2026-06-03 11:43:05 +0530

    Remove commented-out login tests
    Removed commented-out tests for valid and invalid login scenarios using POM.

--- Saucelabs_tests/LoginPOM.spec.js
+++ Saucelabs_tests/LoginPOM.spec.js
@@ -2,25 +2,6 @@ import {test,expect} from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage'
 import LoginData  from '../testData/LoginData.json'
 
-/*test('Valid TestLogin using POM file',async({page})=>{
-    
-    const Login =new LoginPage(page)
-
-    await  Login.gotoLoginPage('https://www.saucedemo.com/')
-    await Login.LoginPage(LoginData.validUser.username,LoginData.validUser.password)
-    await expect(page.getByText(LoginData.validUser.asserttext)).toBeVisible
-
-})
-
-test('InValid TestLogin using POM file',async({page})=>{
-    
-    const Login =new LoginPage(page)
-
-    await  Login.gotoLoginPage('https://www.saucedemo.com/')
-    await Login.LoginPage(LoginData.InvalidUser.username,LoginData.InvalidUser.password)
-    await expect(page.getByText(LoginData.InvalidUser.asserttext)).toBeVisible
-
-})*/
 test('Login',async({page})=>{
     const Login =new LoginPage(page)
     

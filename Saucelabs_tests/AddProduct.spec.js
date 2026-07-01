4795331391e52dfdd6096a2acd68431a65f82669
sudeepini <sudeepinib@gmail.com>
2026-07-01 12:07:49 +0530

    Added Checkout page and chnaged AddProdcut page.

--- Saucelabs_tests/AddProduct.spec.js
+++ Saucelabs_tests/AddProduct.spec.js
@@ -1,7 +1,8 @@
 import {test,expect} from '@playwright/test'
-import { InventoryPage } from '../pages/InventoryPage'
+import { InventoryPage } from '../pages/InventoryPage.js'
 import { LoginPage } from '../pages/LoginPage'  
 import { CartPage } from '../pages/CartPage'
+import { CheckOutPage } from '../pages/CheckOutPage'
 
 test.describe('Products page', ()=>{
 
@@ -12,22 +13,43 @@ test.describe('Products page', ()=>{
         await expect(page.getByText('Products')).toBeVisible()
     })
     test('Add product',async({page})=>{
-        //await page.pause()
         const product=new InventoryPage(page)
         const verify=new CartPage(page)
         let cartcount= await product.getCartCount()
         await product.addProduct('sauce-labs-backpack')
+        await product.addProduct('sauce-labs-bike-light')
         expect( await product.getCartCount()).toBeGreaterThan(cartcount)
         await verify.VerifyProduct('sauce Labs Backpack')
-    })
-
-   test('Remove Product',async({page})=>{
+    })   
+    
+    test('Remove Product',async({page})=>{
         const product=new InventoryPage(page)
         await product.addProduct('sauce-labs-backpack')
             
         await product.removeProduct('sauce-labs-backpack')
     
         await expect(page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')).toBeVisible()
+})
+
+    test('CheckOut Products',async({page})=>{
+        const product=new InventoryPage(page)
+        const checkout=new CheckOutPage(page)
+        const cart=new CartPage(page)
+        await product.addProduct('sauce-labs-backpack')
+        await product.addProduct('sauce-labs-bike-light')
+        await product.openCart()
+        await cart.Continueshopping()
+        await expect(page.locator('[data-test="title"]')).toHaveText('Products')
+        await product.openCart()
+        await cart.checkOut()
+        await checkout.cancelCheckout()
+        await cart.checkOut()
+        await checkout.proceedCheckout('sruthi','Hasan','5444004')
+        await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Overview')
+        expect(await checkout.getSubtotalPrice()).toBe(39.98)
+        expect(await checkout.getTax()).toBe(3.20)
+        expect(await checkout.getTotalPrice()).toBe(43.18)
     })
+
 
 })
